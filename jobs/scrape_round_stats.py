b504361c9c049ba0d2140e57fd71fb12450fa060
David543M <davidmaze742@gmail.com>
2026-04-18 01:32:35 +0100

    Rename SUPABASE_SERVICE_KEY to SUPABASE_KEY

--- jobs/scrape_round_stats.py
+++ jobs/scrape_round_stats.py
@@ -63,9 +63,9 @@ def normalize_name_key(name: str) -> str:
 
 def get_supabase() -> Client:
     url = os.environ.get("SUPABASE_URL", "")
-    key = os.environ.get("SUPABASE_SERVICE_KEY", "")
+    key = os.environ.get("SUPABASE_KEY", "")
     if not url or not key:
-        log.error("SUPABASE_URL and SUPABASE_SERVICE_KEY must be set")
+        log.error("SUPABASE_URL and SUPABASE_KEY must be set")
         sys.exit(1)
     return create_client(url, key)
 
